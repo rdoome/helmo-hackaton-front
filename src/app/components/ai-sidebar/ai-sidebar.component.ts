f657e36780bcf138398debdfd4df7f038cd54fc1
pavel <pavel.deleersnyder@student.howest.be>
2025-04-12 15:43:06 +0200

    insert user prompts into the messages

--- src/app/components/ai-sidebar/ai-sidebar.component.ts
+++ src/app/components/ai-sidebar/ai-sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'ai-sidebar',
@@ -7,14 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './ai-sidebar.component.css'
 })
 export class AiSidebarComponent {
+
+  constructor(private elRef:ElementRef) {}
+
   submitPrompt() {
-    const $input = <HTMLInputElement>document.querySelector("#prompt");
+    const $input = <HTMLInputElement>this.elRef.nativeElement.querySelector("#prompt");
     if (!$input) return;
-    const prompt = $input.value;
+    const prompt = $input.innerText;
     $input.innerText = "";
+    this.addMessage(prompt);
   }
 
   keyDown($event: KeyboardEvent) {
     if ($event.key == "Enter") this.submitPrompt();
+  }
+
+  addMessage(message: string) {
+    this.elRef.nativeElement.querySelector("ul").insertAdjacentHTML('beforeend', `<p>${message}</p>`);
   }
 }
