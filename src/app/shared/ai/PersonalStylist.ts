6dd436e3c79384c5c19b6a4bf22b80858cf03762
pavel <pavel.deleersnyder@student.howest.be>
2025-04-12 10:45:12 +0200

    improve the prompts and messages so the llm understands how it needs to respond

--- src/app/shared/ai/PersonalStylist.ts
+++ src/app/shared/ai/PersonalStylist.ts
@@ -2,17 +2,20 @@ import { OllamaLLM } from "./OllamaLLM";
 
 const MODEL: string = "llama3.2";
 // You can prompt engineer this further if that is needed
-const SYSTEM_PROMPT: string = "You are a personal shop assistent for a fashion company, you want to help the customers to the best of your abilities.";
+const SYSTEM_PROMPT: string = `You are a personal shop assistent for a fashion company, you want to help the customers to the best of your abilities.
+Always give very short responses directly to the customer, do not contain metadata in your completion.
+In Our store we have the following items:
+`;
 
 export class PersonalStylist {
     private readonly _aiModel: OllamaLLM;
 
     constructor(articlesInJson: string) {
-        this._aiModel = new OllamaLLM(MODEL, SYSTEM_PROMPT);
+        this._aiModel = new OllamaLLM(MODEL, SYSTEM_PROMPT + articlesInJson);
     }
 
     public async addArticleToCart(article: string): Promise<string> {
-        const prompt = `The customer has put ${article} in their cart, Please provide them with some usefull suggestions for other products they might like.`;
+        const prompt = `The customer has put ${article} in their cart, Please provide them with some usefull suggestions for other products they might like. Include the Id's of the recommended products as follows \${id}`;
         return this._aiModel.generate(prompt);
     }
 
