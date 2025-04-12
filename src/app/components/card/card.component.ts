8046c60a3c83c63a7f5b9810530906004077523e
Maes Theo <t.maes@student.helmo.be>
2025-04-13 00:51:00 +0200

    removeFromCart

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,5 +1,5 @@
-import { Component, inject, input, OnInit } from '@angular/core';
-import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
+import {Component, inject, input, Input} from '@angular/core';
+import { faCartPlus, faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { CartService } from '../../store/cart/cart.store';
@@ -16,17 +16,25 @@ export class CardComponent {
   id = input.required<string>();
   imageUrl = input.required<string>();
   category = input.required<string>();
-  displayAddToCart = input<boolean>(true);
+  actionButton= input.required<'addToCart' | 'removeFromCart' | ''>();
   title = input.required<string>();
   private cartService = inject(CartService);
   products = CARDIGAN_COLLECTION;
 
   faCartPlus = faCartPlus;
+  faCircleXMark = faCircleXmark;
 
   addToCart() {
     const product = this.products.find(product => product.id === this.id());
     if (product) {
       this.cartService.addProductToCart(product);
+    }
+  }
+
+  removeFromCart(){
+    const product = this.products.find(product => product.id === this.id());
+    if (product) {
+      this.cartService.deleteProductFromCart(product);
     }
   }
 }
